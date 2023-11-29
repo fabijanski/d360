30018a712196d51032e42e41fb4b0464cd88d235
Michał <mm.fabijanski@gmail.com>
2023-11-29 12:42:56 +0100

    Add button variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,32 @@
 import { PropsWithChildren } from "react"
 
-type Props = {
+type VariantType = "primary" | "secondary"
+
+interface ButtonProps {
   onClick?: React.MouseEventHandler<HTMLElement>
+  className?: string
+  variant?: VariantType
 }
 
 export const Button = ({
   children,
   onClick,
+  className = "",
+  variant = "primary",
   ...rest
-}: PropsWithChildren<Props>) => (
-  <button
-    className="bg-white text-primary px-[3.5rem] py-[1.4rem] rounded-[1.4rem]"
-    onClick={onClick}
-    {...rest}
-  >
-    {children}
-  </button>
-)
+}: PropsWithChildren<ButtonProps>) => {
+  const variantClassName = {
+    primary: "bg-white text-primary",
+    secondary: "bg-transparent text-white",
+  }[variant]
+
+  return (
+    <button
+      className={`px-[3.5rem] py-[1.4rem] rounded-[1.4rem] ${variantClassName} ${className}`}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
