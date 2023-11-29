4ca81d9124e297d80109f6c12d434fb2d0b01949
Michał <mm.fabijanski@gmail.com>
2023-11-29 22:48:57 +0100

    Add basePath for prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production"
 const nextConfig = {
+  basePath: isProduction ? "/d360" : "",
   output: "export",
   distDir: "dist",
   images: {
